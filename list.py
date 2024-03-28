19e0d3e31f28ac974a2d30f6963a8179e870be0a
maneesha <maneesha@abntechcorp.com>
2024-03-28 22:15:02 +0530

    program on dict

--- list.py
+++ list.py
@@ -1,6 +1,11 @@
-l=[2,4,5,3,7,9,5,6]
+'''l=[2,4,5,3,7,9,5,6]
 l1=[]
+l2=[]
 for i in l:
     if i%2==0:
         l1.append(i)
-print(l1)
+    else:
+        l2.append(i)
+print(l1)'''
+dict={"fruit":"apple","vitamin":"A","origin":"shimla","calories":"20cal"}
+print(dict["fruit"])
